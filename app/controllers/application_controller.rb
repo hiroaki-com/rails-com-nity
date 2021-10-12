51a55e5814875fa41cbd6f077b85bc69e6fc723e
hiroaki-com <comukichi@gmail.com>
2021-10-12 17:03:06 +0900

    first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  
+  def after_sign_out_path_for(resource)
+    new_user_session_path # ログアウト後に遷移するpathを設定
+  end
+
 end
