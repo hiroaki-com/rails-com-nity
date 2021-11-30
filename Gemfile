758bccb2a67fd16a0b7018d16095b4755deb4c92
hiroaki-com <comukichi@gmail.com>
2021-11-30 23:28:59 +0900

    adminlink added

--- Gemfile
+++ Gemfile
@@ -49,3 +49,6 @@ end
 
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+
+gem 'rails_admin', '~> 2.0.0.beta'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   resources :posts
   resources :profiles, only: [:show, :new, :edit, :create, :update]
